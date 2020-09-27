2e6bc81358239b64a4b052b846cc528aaa522164
zaid kamil <xaidmetamorphos@gmail.com>
2020-09-27 15:28:10 +0530

    auth update # 2

--- app/src/main/java/digipodium/authenticationdemo/auth/AuthChoiceFragment.java
+++ app/src/main/java/digipodium/authenticationdemo/auth/AuthChoiceFragment.java
@@ -2,65 +2,40 @@ package digipodium.authenticationdemo.auth;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import digipodium.authenticationdemo.FirstFragment;
 import digipodium.authenticationdemo.R;
+import digipodium.authenticationdemo.databinding.FragmentAuthChoiceBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AuthChoiceFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AuthChoiceFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AuthChoiceFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AuthChoiceFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AuthChoiceFragment newInstance(String param1, String param2) {
-        AuthChoiceFragment fragment = new AuthChoiceFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FragmentAuthChoiceBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_auth_choice, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentAuthChoiceBinding.bind(view);
+
+        binding.btnLogin.setOnClickListener(v1 -> {
+            NavHostFragment.findNavController(this).navigate(R.id.action_authChoiceFragment_to_loginFragment);
+        });
+        binding.btnRegister.setOnClickListener(v2 -> {
+            NavHostFragment.findNavController(this).navigate(R.id.action_authChoiceFragment_to_registerFragment);
+        });
     }
 }
--- app/src/main/java/digipodium/authenticationdemo/auth/LoginFragment.java
+++ app/src/main/java/digipodium/authenticationdemo/auth/LoginFragment.java
@@ -2,65 +2,53 @@ package digipodium.authenticationdemo.auth;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import digipodium.authenticationdemo.R;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import digipodium.authenticationdemo.R;
+import digipodium.authenticationdemo.databinding.FragmentLoginBinding;
+
+
 public class LoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FragmentLoginBinding binding ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentLoginBinding.bind(view);
+
+        binding.btnCreateAcc.setOnClickListener(v1 ->{
+            NavHostFragment.findNavController(this).navigate(R.id.action_loginFragment_to_registerFragment);
+        });
+
+        binding.btnForgot.setOnClickListener(v2->{
+            NavHostFragment.findNavController(this).navigate(R.id.action_loginFragment_to_forgotPassFragment);
+        });
+
+        binding.btnLogin2.setOnClickListener(v3->{
+            binding.progressBar.setVisibility(View.VISIBLE);
+            String email = binding.editEmail.getText().toString();
+            String password = binding.editPassword.getText().toString();
+            Snackbar.make(v3,"still working", Snackbar.LENGTH_LONG).show();
+        });
+
+
     }
 }
